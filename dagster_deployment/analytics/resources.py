cbd426e40343ec95b899a1d8d3dafefdf44e1563
Fahad Shoaib <fahadshoaib15@gmail.com>
2025-04-13 03:02:10 +0500

    removed volume mounts. Used BigQueryResource gcp_creds parameter.

--- dagster_deployment/analytics/resources.py
+++ dagster_deployment/analytics/resources.py
@@ -3,5 +3,6 @@ from dagster_gcp import BigQueryResource
 
 # Define BigQuery resource with project ID and credentials path
 bigquery_resource = BigQueryResource(
-    project=EnvVar("GCP_PROJECT_ID").get_value(),
+    project=EnvVar("GCP_PROJECT_ID"),
+    gcp_credentials=EnvVar("GCP_CREDS"),
 )
--- dagster_deployment/ingestion/resources.py
+++ dagster_deployment/ingestion/resources.py
@@ -4,4 +4,5 @@ from dagster_gcp import BigQueryResource
 
 bigquery_resource = BigQueryResource(
     project=EnvVar("GCP_PROJECT_ID"),
+    gcp_credentials=EnvVar("GCP_CREDS"),
 )
